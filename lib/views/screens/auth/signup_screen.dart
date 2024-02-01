3e92d052f849c81423a7f89ac03fe2f6d9d343fd
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-01 17:29:49 +0900

    新規登録画面のUI作成

---
+++ lib/views/screens/auth/signup_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/constants.dart';
+import 'package:tiktok_flutter/views/widgets/text_input_field.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<SignupScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Tiktok Clone',
+              style: TextStyle(
+                color: buttonColor,
+                fontSize: 35,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            const Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 64,
+                  backgroundColor: Colors.black,
+                  backgroundImage: NetworkImage(
+                    'https://clipart-library.com/new_gallery/280-2806732_png-file-svg-default-profile-picture-png.png',
+                  ),
+                ),
+                Positioned(
+                  left: 80,
+                  bottom: -10,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: TextInputField(
+                labelText: 'Username',
+                controller: _usernameController,
+                icon: const Icon(Icons.person),
+                isObscure: false,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              width: MediaQuery.of(context).size.width,
+              child: TextInputField(
+                labelText: 'Email',
+                controller: _emailController,
+                icon: const Icon(Icons.email),
+                isObscure: false,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: TextInputField(
+                labelText: 'Password',
+                controller: _passwordController,
+                icon: const Icon(Icons.password),
+                isObscure: true,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 50,
+              decoration: BoxDecoration(
+                color: buttonColor,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: InkWell(
+                onTap: () {
+                  print('signup');
+                },
+                child: const Center(
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  // バックスラッシュで点があってもシングルクォーテーションで書ける
+                  'Already have an account?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    print('navi login');
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: buttonColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
