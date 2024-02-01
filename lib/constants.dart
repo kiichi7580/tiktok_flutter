3bf98a4c8be49796cd13a0e9d0c97f15afec5258
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-01 22:39:53 +0900

    firebase関連の変数設定

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/controllers/auth_controller.dart';
 
 // COLORS
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
+
+// firebase関連
+var firebaseAuth = FirebaseAuth.instance;
+var firebaseStorage = FirebaseStorage.instance;
+var firestore = FirebaseFirestore.instance;
+
+// controller関連
+var authController = AuthController.instance;
