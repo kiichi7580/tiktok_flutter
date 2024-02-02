8ed348eb2ff5337a1f51438573b3f959e6746008
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-02 23:09:48 +0900

    ボトムナビゲーションバーを作成、選択時にアイコンの色が変更されるようにした

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/constants.dart';
+import 'package:tiktok_flutter/views/widgets/custom_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _pageIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
+
+  void onChangedPage(int page) {}
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: _onItemTapped,
+        backgroundColor: backgroundColor,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Search',
+            icon: Icon(
+              Icons.search,
+              size: 30,
+            ),
+          ),
+          BottomNavigationBarItem(label: '', icon: CustomIcon()),
+          BottomNavigationBarItem(
+            label: 'Messages',
+            icon: Icon(
+              Icons.message,
+              size: 30,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(
+              Icons.person,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+      body: pages[_pageIndex],
     );
   }
 }
