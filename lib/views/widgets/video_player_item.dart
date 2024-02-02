77d7a03c9e806386531bbc938ce319bae2966756
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-03 02:44:17 +0900

    networkUrlだと処理がされないので、非推奨だけどnetworkを使う

--- lib/views/widgets/video_player_item.dart
+++ lib/views/widgets/video_player_item.dart
@@ -15,7 +15,7 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
   @override
   void initState() {
     super.initState();
-    videoPlayerController = VideoPlayerController.networkUrl(widget.videoUrl)
+    videoPlayerController = VideoPlayerController.network(widget.videoUrl)
       ..initialize().then((value) {
         videoPlayerController.play();
         videoPlayerController.setVolume(1);
