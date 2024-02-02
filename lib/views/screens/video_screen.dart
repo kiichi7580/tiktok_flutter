1717687dfb5444495e5b021aed90491ca194fc93
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-03 02:42:48 +0900

    ホーム画面のUI作成、取得した動画一覧を背景に表示させる処理を追加

--- lib/views/screens/video_screen.dart
+++ lib/views/screens/video_screen.dart
@@ -1,103 +1,245 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/controllers/video_controller.dart';
+import 'package:tiktok_flutter/views/widgets/circle_animation.dart';
 import 'package:tiktok_flutter/views/widgets/video_player_item.dart';
+import 'package:get/get.dart';
 
 class VideoScreen extends StatelessWidget {
-  const VideoScreen({super.key});
+  VideoScreen({super.key});
+
+  final VideoController videoController = Get.put(VideoController());
+
+  buildProfile(String profilePhoto) {
+    return SizedBox(
+      height: 60,
+      width: 60,
+      child: Stack(
+        children: [
+          Positioned(
+            left: 5,
+            child: Container(
+              height: 50,
+              width: 50,
+              padding: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Image(
+                  image: NetworkImage(
+                    profilePhoto,
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildMusicAlbum(String profilePhoto) {
+    return SizedBox(
+      height: 60,
+      width: 60,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(11),
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.grey,
+                  Colors.white,
+                ],
+              ),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image(
+                image: NetworkImage(
+                  profilePhoto,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: PageView.builder(
-        // itemCount: ,
-        controller: PageController(
-          initialPage: 0,
-          viewportFraction: 1,
-        ),
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) {
-          return Stack(
-            children: [
-              // VideoPlayerItem(
-              // videoUrl: ,
-              // ),
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Expanded(
-                      child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.only(
-                            left: 20,
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                'username',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
+      body: Obx(() {
+        return PageView.builder(
+          itemCount: videoController.videoList.length,
+          controller: PageController(
+            initialPage: 0,
+            viewportFraction: 1,
+          ),
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            final data = videoController.videoList[index];
+            return Stack(
+              children: [
+                VideoPlayerItem(
+                  videoUrl: data.videoUrl,
+                ),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    Expanded(
+                        child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            padding: const EdgeInsets.only(
+                              left: 20,
+                            ),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  data.username,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                'caption',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.music_note,
-                                    size: 15,
+                                Text(
+                                  data.caption,
+                                  style: const TextStyle(
+                                    fontSize: 15,
                                     color: Colors.white,
                                   ),
-                                  Text(
-                                    'song name',
-                                    style: TextStyle(
-                                      fontSize: 15,
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.music_note,
+                                      size: 15,
                                       color: Colors.white,
-                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    Text(
+                                      data.songname,
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          margin: EdgeInsets.only(
+                            top: size.height / 5,
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildProfile(data.profilePhoto),
+                              Column(
+                                children: [
+                                  InkWell(
+                                    onTap: () {},
+                                    child: const Icon(
+                                      Icons.favorite,
+                                      size: 40,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 7,
+                                  ),
+                                  Text(
+                                    data.likes.length.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
                                     ),
                                   ),
                                 ],
+                              ),
+                              Column(
+                                children: [
+                                  InkWell(
+                                    onTap: () {},
+                                    child: const Icon(
+                                      Icons.comment,
+                                      size: 40,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 7,
+                                  ),
+                                  Text(
+                                    data.commentCount.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  InkWell(
+                                    onTap: () {},
+                                    child: const Icon(
+                                      Icons.reply,
+                                      size: 40,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 7,
+                                  ),
+                                  Text(
+                                    data.shareCount.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              CircleAnimation(
+                                child: buildMusicAlbum(data.profilePhoto),
                               )
                             ],
                           ),
                         ),
-                      ),
-                      Container(
-                        width: 100,
-                        margin: EdgeInsets.only(
-                          top: size.height / 5,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            
-                          ],
-                        ),
-                      ),
-                    ],
-                  ))
-                ],
-              )
-            ],
-          );
-        },
-      ),
+                      ],
+                    ))
+                  ],
+                )
+              ],
+            );
+          },
+        );
+      }),
     );
   }
 }
