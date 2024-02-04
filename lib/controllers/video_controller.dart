c0ecc9593ea8556d705f7818d3c49da155e38fef
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-04 19:23:00 +0900

    動画にいいねができる機能追加

--- lib/controllers/video_controller.dart
+++ lib/controllers/video_controller.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_flutter/constants.dart';
 import 'package:tiktok_flutter/models/video.dart';
+import 'package:tiktok_flutter/controllers/auth_controller.dart';
 
 class VideoController extends GetxController {
   final Rx<List<Video>> _videoList = Rx<List<Video>>([]);
@@ -19,5 +20,19 @@ class VideoController extends GetxController {
       }
       return retVal;
     }));
+  }
+
+  likeVideo(String id) async {
+    DocumentSnapshot doc = await firestore.collection('videos').doc(id).get();
+    var uid = authController.user.uid;
+    if ((doc.data()! as dynamic)['likes'].contains(uid)) {
+      await firestore.collection('videos').doc(id).update({
+        'likes': FieldValue.arrayRemove([uid]),
+      });
+    } else {
+      await firestore.collection('videos').doc(id).update({
+        'likes': FieldValue.arrayUnion([uid]),
+      });
+    }
   }
 }
