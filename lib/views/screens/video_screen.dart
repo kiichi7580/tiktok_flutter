13b47e25e1bfd9cacc1327e95c776707ceb05eb0
kiichi7580 <kiichi.nakazato0521@gmail.com>
2024-02-04 19:30:20 +0900

    いいね機能追加、コメントボタンでコメントできるページに遷移する

--- lib/views/screens/video_screen.dart
+++ lib/views/screens/video_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter/constants.dart';
 import 'package:tiktok_flutter/controllers/video_controller.dart';
+import 'package:tiktok_flutter/views/screens/comment_screen.dart';
 import 'package:tiktok_flutter/views/widgets/circle_animation.dart';
 import 'package:tiktok_flutter/views/widgets/video_player_item.dart';
 import 'package:get/get.dart';
@@ -162,11 +164,16 @@ class VideoScreen extends StatelessWidget {
                               Column(
                                 children: [
                                   InkWell(
-                                    onTap: () {},
-                                    child: const Icon(
+                                    onTap: () =>
+                                        videoController.likeVideo(data.id),
+                                    child: Icon(
                                       Icons.favorite,
                                       size: 40,
-                                      color: Colors.red,
+                                      color: data.likes.contains(
+                                        authController.user.uid,
+                                      )
+                                          ? Colors.red
+                                          : Colors.white,
                                     ),
                                   ),
                                   const SizedBox(
@@ -184,7 +191,12 @@ class VideoScreen extends StatelessWidget {
                               Column(
                                 children: [
                                   InkWell(
-                                    onTap: () {},
+                                    onTap: () => Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            CommentScreen(id: data.id),
+                                      ),
+                                    ),
                                     child: const Icon(
                                       Icons.comment,
                                       size: 40,
